c71c14f3824fe9f53b605efa59512a87f851dd93
duna <841914212@qq.com>
2020-07-20 21:59:49 +0800

    git commit -m 'StackedAreaChart'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Demo from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { StackedAreaChart } from 'bizcharts';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <Demo />
   </React.StrictMode>,
   document.getElementById('root')
 );
